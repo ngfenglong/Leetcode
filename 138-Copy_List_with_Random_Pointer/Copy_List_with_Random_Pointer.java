c84cbc745efb8971a79c1c030cd02d67b27e0290
fenglong_ng <fenglong1502@hotmail.com>
2023-09-05 08:35:04 +0800

    138 Copy List with Random Pointer java solution

---
+++ 138-Copy_List_with_Random_Pointer/Copy_List_with_Random_Pointer.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        HashMap<Node, Node> mapping = new HashMap<>();
+        
+        Node newHead = new Node(head.val);
+        Node newTemp = newHead;
+        Node temp = head;
+        mapping.put(head, newHead);
+        
+        while(temp.next != null){
+            newTemp.next = new Node(temp.next.val);
+            
+            temp = temp.next;
+            newTemp = newTemp.next;
+            mapping.put(temp, newTemp);
+        }
+        
+        temp = head;
+        newTemp = newHead;
+        while(temp != null){
+            if(temp.random != null){
+                newTemp.random = mapping.get(temp.random);
+            }
+            
+            newTemp = newTemp.next;
+            temp = temp.next;
+        }
+        
+        return newHead;
+    }
+}
