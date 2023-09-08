e2b5596ec993bde70ea3a829ad411ea9e51299cb
fenglong_ng <fenglong1502@hotmail.com>
2023-09-08 08:16:04 +0800

    118 - Pascal Triangle go solution

---
+++ 118-Pascal_Triangle/Pascal_Triangle.go
@@ -0,0 +1,19 @@
+package pascaltriangle
+
+func generate(numRows int) [][]int {
+	ans := make([][]int, numRows)
+
+	for i := 1; i <= numRows; i++ {
+		ans[i-1] = make([]int, i)
+
+		for j := 0; j < i; j++ {
+			if j == 0 || j == i-1 {
+				ans[i-1][j] = 1
+			} else {
+				ans[i-1][j] = ans[i-2][j-1] + ans[i-2][j]
+			}
+		}
+	}
+
+	return ans
+}
