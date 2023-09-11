af355644179375ba028f8707ae2a56d64155b245
fenglong_ng <fenglong1502@hotmail.com>
2023-09-11 08:37:42 +0800

    1282 Group the people java solution

---
+++ 1282-Group_the_People_Given_the_Group_Size_They_Belong_To/Group_the_People_Given_the_Group_Size_They_Belong_To.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        HashMap<Integer, List<Integer>> hm = new HashMap<>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        
+        // O(n)        
+        for(int i = 0; i < groupSizes.length; i++){
+            int size = groupSizes[i];
+            if(!hm.containsKey(size)) hm.put(size, new ArrayList<>());
+            hm.get(size).add(i);
+            
+            if(hm.get(size).size() == size){
+                ans.add(hm.get(size));
+                hm.put(size, new ArrayList<>());
+            }
+        }
+        
+        return ans;
+    }
+}
