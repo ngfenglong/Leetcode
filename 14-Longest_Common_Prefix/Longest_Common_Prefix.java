134415d6428193e8f88d84e254e321442c61eb09
fenglong_ng <fenglong1502@hotmail.com>
2023-09-14 08:18:52 +0800

    Longest Common Prefix java solution

---
+++ 14-Longest_Common_Prefix/Longest_Common_Prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        String firstWord = strs[0];
+        
+        char c;
+        
+        for(int j = 0; j < firstWord.length(); j++){
+            c = firstWord.charAt(j);
+            
+            for(int i = 0; i < strs.length; i++){
+               if(j >= strs[i].length() || strs[i].charAt(j) != c) return sb.toString();
+            }
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
