d73147d0c6792019d22ecf6afc5e8e499b53c54d
fenglong_ng <fenglong1502@hotmail.com>
2023-09-18 09:07:22 +0800

    1337 - K weakest rows in a matrix java solution

--- 1337-The_K_Weakest_Rows_in_a_Matrix/The_K_Weakest_Rows_in_a_Matrix.java
+++ 1337-The_K_Weakest_Rows_in_a_Matrix/The_K_Weakest_Rows_in_a_Matrix.java
@@ -4,7 +4,7 @@ class Solution {
         
         for(int i = 0 ; i < mat.length; i++){
             int count = 0;
-            for(int j = 0; j < mat[0].length; j++){
+            for(int j = 0; j < mat[0].length; j++)
                 if(mat[i][j] != 1) break;
                 count++;
             }
