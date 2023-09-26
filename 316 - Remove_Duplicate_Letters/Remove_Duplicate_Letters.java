50b5c5464e7f7aee666c65fdb3029ba038bdaa8e
fenglong_ng <fenglong1502@hotmail.com>
2023-09-26 08:22:05 +0800

    316 Remove Duplicate Letters java solution

---
+++ 316 - Remove_Duplicate_Letters/Remove_Duplicate_Letters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        // get the list of duplicates & the index/ the number of dups
+        
+        int[] counts = new int[26];
+        
+        for(int i = 0; i < s.length(); i++){
+            counts[s.charAt(i) - 'a']++;
+        }
+        
+        int pos = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            counts[s.charAt(i) - 'a']--;
+            if(s.charAt(i) < s.charAt(pos)){
+                pos = i;
+            } 
+            if(counts[s.charAt(i) - 'a'] == 0) break;
+        }
+        
+        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll(""+ s.charAt(pos), ""));
+        
+        
+    }
+}
