3977184ff0f73d2a801517b0ec1c851b369efa1b
fenglong_ng <fenglong1502@hotmail.com>
2023-09-29 08:45:55 +0800

    896 - Monotonic Array java solution

---
+++ 896-Monotonic_Array/Monotonic_Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        Boolean isIncreasing = null;
+        if(nums.length == 1) return true;
+        
+        int prev = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > prev){
+                if(isIncreasing != null && isIncreasing == false) return false;
+                isIncreasing = true;
+            }
+            if(nums[i] < prev){
+                if(isIncreasing != null && isIncreasing == true) return false;
+                isIncreasing = false;
+            }
+            prev = nums[i];
+        }
+        return true;
+    }
+}
