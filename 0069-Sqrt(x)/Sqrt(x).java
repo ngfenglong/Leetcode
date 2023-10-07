f3aeca1485b05c8bdbfb077931f579af7f6db4d8
fenglong_ng <fenglong1502@hotmail.com>
2023-10-07 18:40:59 +0800

    69 Sqrt(x) java solution

---
+++ 0069-Sqrt(x)/Sqrt(x).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        if(x < 4) return 1;
+        
+        int left = 1;
+        int right = x;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if((long)mid * mid == x) return mid;
+            else if((long)mid*mid > x)  right = mid - 1;
+            else left = mid + 1;
+        }
+            
+        return right;
+    }
+}
