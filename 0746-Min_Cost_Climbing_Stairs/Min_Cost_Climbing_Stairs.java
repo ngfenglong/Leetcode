5eafad3f6e894d67178a48fdadc2a27f59c46b61
fenglong_ng <fenglong1502@hotmail.com>
2023-10-13 08:19:50 +0800

    746 - Min Cost Climbing Stairs java solution

---
+++ 0746-Min_Cost_Climbing_Stairs/Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] totalCost = new int[cost.length + 1];
+        for(int i = 2; i < totalCost.length; i++){
+            totalCost[i] = Math.min(totalCost[i - 1] + cost[i - 1], totalCost[i - 2] + cost[i - 2]);
+        }
+        
+        return totalCost[cost.length];
+    }
+}
