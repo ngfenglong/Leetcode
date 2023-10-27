050fa9b5dd8def87c568b0e45a5d7e2e1589ebbd
fenglong_ng <fenglong1502@hotmail.com>
2023-10-27 08:18:40 +0800

    5 - Longest Palindromic Substring java solution

---
+++ 0005-Longest_Palindromic_Substring/Longest_Palindromic_Substring.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String ans = "";
+        for(int i = 0; i < s.length(); i++){
+            String s1 = checkPalindromeLength(s, i, i);
+            String s2 = checkPalindromeLength(s, i - 1, i);
+            
+            if(s1.length() > ans.length()) ans = s1;
+            if(s2.length() > ans.length()) ans = s2;
+        }
+        return ans;
+    }
+    
+    private String checkPalindromeLength(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(right) == s.charAt(left)) {
+            left--;
+            right++;
+        }
+        
+        return s.substring(left + 1, right);
+    }
+}
+
+
