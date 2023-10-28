a6dc4bab45ca0b27c23342d2141455d297edcf06
fenglong_ng <fenglong1502@hotmail.com>
2023-10-28 11:11:57 +0800

    383 Ransom Note java solution

---
+++ 0383-Ransom_Note/Ransom_Note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+        for(int j = 0; j < magazine.length(); j++){
+            int c = magazine.charAt(j) - 'a';
+            letters[c]++;
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++){
+            int c = ransomNote.charAt(i) - 'a';
+            if(letters[c] == 0) return false;
+            letters[c]--;
+        }
+        
+        return true;
+    }
+}
