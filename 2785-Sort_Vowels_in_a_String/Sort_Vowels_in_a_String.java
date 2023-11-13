d9af470953e60e2ac39b3171d0b2591d0e6ad112
fenglong_ng <fenglong1502@hotmail.com>
2023-11-13 12:56:46 +0800

    2785. Improve on the java solution

--- 2785-Sort_Vowels_in_a_String/Sort_Vowels_in_a_String.java
+++ 2785-Sort_Vowels_in_a_String/Sort_Vowels_in_a_String.java
@@ -1,72 +1,43 @@
 class Solution {
     public String sortVowels(String s) {
-        int[] vowels = new int[10];
-        Set<Character> vowelSet = new HashSet<>();
-        HashMap<Integer, Character> intToChar = new HashMap<>();
-        HashMap<Character, Integer> charToInt = new HashMap<>();
+        int[] vowels = new int[10]; // Array to count vowels
         
-        setupVowels(vowelSet, intToChar, charToInt);
-        
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(vowelSet.contains(c)){
-                int idx = charToInt.get(c);
+        for (char c : s.toCharArray()) {
+            int idx = getVowelIndex(c);
+            if (idx != -1) {
                 vowels[idx]++;
             }
         }
-        
-        
-        int currIdx = 0;
+
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(vowelSet.contains(c)){
-                while(vowels[currIdx] == 0){
-                    currIdx++;
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                for (int i = 0; i < vowels.length; i++) {
+                    if (vowels[i] > 0) {
+                        sb.append(getVowelFromIndex(i));
+                        vowels[i]--;
+                        break;
+                    }
                 }
-                vowels[currIdx]--;
-                sb.append(intToChar.get(currIdx));
-            }
-            else{
+            } else {
                 sb.append(c);
             }
         }
-        
+
         return sb.toString();
     }
-    
-    private void setupVowels(Set<Character> set, HashMap<Integer, Character> intToChar, HashMap<Character, Integer> charToInt ){
-        set.add('A');
-        set.add('E');
-        set.add('I');
-        set.add('O');
-        set.add('U');
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-        
-        charToInt.put('A',0);
-        charToInt.put('E',1);
-        charToInt.put('I',2);
-        charToInt.put('O',3);
-        charToInt.put('U',4);
-        charToInt.put('a',5);
-        charToInt.put('e',6);
-        charToInt.put('i',7);
-        charToInt.put('o',8);
-        charToInt.put('u',9);
-        
-        intToChar.put(0,'A');
-        intToChar.put(1,'E');
-        intToChar.put(2,'I');
-        intToChar.put(3,'O');
-        intToChar.put(4,'U');
-        intToChar.put(5,'a');
-        intToChar.put(6,'e');
-        intToChar.put(7,'i');
-        intToChar.put(8,'o');
-        intToChar.put(9,'u');
+
+    private boolean isVowel(char c) {
+        return "AEIOUaeiou".indexOf(c) >= 0;
+    }
+
+    private int getVowelIndex(char c) {
+        String vowels = "AEIOUaeiou";
+        return vowels.indexOf(c);
+    }
+
+    private char getVowelFromIndex(int idx) {
+        String vowels = "AEIOUaeiou";
+        return vowels.charAt(idx);
     }
 }
