9d2aa74a106a57e2fca799366586584d4aac2e73
Zell <fenglong1502@hotmail.com>
2024-01-02 08:30:59 +0800

    2610 Convert an Array into 2D Array With Conditions java solution

---
+++ 2610-Convert_an_Array_Into_2D_Array_With_Conditions/Convert_an_Array_Into_2D_Array_With_Conditions.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i: nums){
+            if(freq[i] >= ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+            ans.get(freq[i]).add(i);
+            freq[i]++;
+        }
+        
+        return ans;
+    }
+}
