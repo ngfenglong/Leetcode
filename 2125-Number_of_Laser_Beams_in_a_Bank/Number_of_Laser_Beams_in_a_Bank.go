42c3c5805f076c71810c76efe8819acf49b3550c
Zell <fenglong1502@hotmail.com>
2024-01-03 08:20:10 +0800

    2125 Numnber of Laser Beams in a Bank go solution

---
+++ 2125-Number_of_Laser_Beams_in_a_Bank/Number_of_Laser_Beams_in_a_Bank.go
@@ -0,0 +1,23 @@
+package numberoflaserbeamsinabank
+
+func numberOfBeams(bank []string) int {
+	var prev, ans int
+
+	for _, str := range bank {
+		count := 0
+
+		byteStr := []byte(str)
+		for _, b := range byteStr {
+			if b == '1' {
+				count++
+			}
+		}
+
+		if count > 0 {
+			ans += (prev * count)
+			prev = count
+		}
+	}
+
+	return ans
+}
