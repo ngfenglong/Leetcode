2078ac9b91d9c2e2e2c3d0cc5cb654222e5322ec
Zell <fenglong1502@hotmail.com>
2024-01-17 13:57:06 +0800

    1207 Number of Occurance java solution

---
+++ 1207-Unique_Number_of_Occurrences/Unique_Number_of_Occurrences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for (int i = 0; i < arr.length; i++){
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        }
+        
+        Set<Integer> occur = new HashSet<>();
+        for(Integer key: hm.keySet()) {
+            int val = hm.get(key);
+            if(occur.contains(val)) return false;
+            
+            occur.add(val);
+        }
+        
+        return true;
+    }
+}
