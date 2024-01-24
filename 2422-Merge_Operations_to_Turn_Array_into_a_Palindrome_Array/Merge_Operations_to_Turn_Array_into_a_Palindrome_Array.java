6ee1f854c8938f3b15072a3acfd1e1715a66eb24
Zell <fenglong1502@hotmail.com>
2024-01-25 07:49:18 +0800

    2422 - Merge Operations to turn array into a palindrome array java solution

---
+++ 2422-Merge_Operations_to_Turn_Array_into_a_Palindrome_Array/Merge_Operations_to_Turn_Array_into_a_Palindrome_Array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int operations = 0;
+        int leftValue = nums[left];
+        int rightValue = nums[right];
+            
+        while(left < right){
+            if(leftValue == rightValue){
+                leftValue = nums[++left];
+                rightValue = nums[--right];
+            }
+            else if(leftValue < rightValue){
+                leftValue += nums[++left];
+                operations++;
+            }
+            else {
+                rightValue += nums[--right];
+                operations++;
+            }
+        }
+        
+        return operations;
+    }
+}
