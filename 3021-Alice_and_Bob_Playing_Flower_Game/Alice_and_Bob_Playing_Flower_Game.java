cbcb266cdbb9f7ac1450c7982c8305c791623309
Zell <fenglong1502@hotmail.com>
2024-01-28 12:05:51 +0800

    3021 - Alice and Bob Playing Flower Game java solution

---
+++ 3021-Alice_and_Bob_Playing_Flower_Game/Alice_and_Bob_Playing_Flower_Game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        if(n+m < 3) return 0l;
+        
+        int evenN = n/2;
+        int oddN = (n + 1) / 2;
+        
+        int evenM = m/2;
+        int oddM = (m + 1) / 2;
+        
+        return ((long)evenN * oddM) + ((long)oddN * evenM);
+        
+    }
+}
