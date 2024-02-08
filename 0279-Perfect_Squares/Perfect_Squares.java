1d475026e7d79ece006a3ada9781fce8ee8b166d
Zell <fenglong1502@hotmail.com>
2024-02-08 08:25:39 +0800

    279 - Perfect Squares java solution

---
+++ 0279-Perfect_Squares/Perfect_Squares.java
@@ -0,0 +1,16 @@
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public int numSquares(int n) {
+        int[] arr = new int[n + 1];
+        
+        for(int i = 1; i <= n; i++){
+            int least = Integer.MAX_VALUE;
+            for(int j = 1; (j * j) <= i; j++){
+                least = Math.min(least, arr[i - (j*j)] + 1);
+            }
+            arr[i] = least;
+        }
+        
+        return arr[n];
+    }
+}
