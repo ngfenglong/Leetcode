6b31a1f3a00bdf00c7eec312755d1849844c0e45
Zell <fenglong1502@hotmail.com>
2024-02-10 19:07:47 +0800

    647 - Palindrome Substrings java solution

---
+++ 0647-Palindrome_Substrings/Palindrome_Substrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    int count = 0;
+    public int countSubstrings(String s) {
+        for(int i = 0; i < s.length(); i++){
+            checkPalindrome(s, i, i);
+            checkPalindrome(s, i, i + 1);
+        }
+        return count;
+    }
+    
+    private void checkPalindrome(String s, int left, int right) {
+        while(left >= 0 && right<= s.length() - 1 && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+            count++;
+        }
+    }
+}
