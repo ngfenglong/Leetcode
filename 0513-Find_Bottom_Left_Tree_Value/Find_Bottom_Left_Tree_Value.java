0d4ef5e2ec73b9242ec1bbb8942774217a6d0737
Zell <fenglong1502@hotmail.com>
2024-02-28 08:38:24 +0800

    513 - Find Bottom Left Tree Value

---
+++ 0513-Find_Bottom_Left_Tree_Value/Find_Bottom_Left_Tree_Value.java
@@ -0,0 +1,35 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int ans = -1;
+    int maxDepth = -1;
+    public int findBottomLeftValue(TreeNode root) {
+        
+        checkBottomLeft(root, 0);
+        
+        return ans;
+    }
+    
+    private void checkBottomLeft(TreeNode root, int level){
+        if(root == null) return;
+        
+        if(level > maxDepth){
+            ans = root.val;
+            maxDepth = level;
+        }
+        
+        checkBottomLeft(root.left, level + 1);
+        checkBottomLeft(root.right, level + 1);
+    }
+}
