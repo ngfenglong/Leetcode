bf74d85236bd0c2e919d9f83d9414da5a93b2ebf
Zell <fenglong1502@hotmail.com>
2024-03-04 08:47:10 +0800

    948 - Bag of Tokens java  solution

---
+++ 0948-Bag_of_Tokens/Bag_of_Tokens.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        if(tokens.length == 0) return 0;
+        
+        Arrays.sort(tokens);
+        int left = 0;
+        int right = tokens.length - 1;
+        int score = 0;
+        
+        while(left < tokens.length && power >= tokens[left]) {
+            power -= tokens[left++];
+            score++;
+        }
+        
+        
+        while(left < right && score > 0){
+            power += tokens[right];
+            score--;
+            while(left < right && power >= tokens[left]){
+                score++;
+                power -= tokens[left++];
+            }
+            right--;
+        }
+        
+        return score;
+    }
+}
