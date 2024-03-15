ee9c638add2d0cbcd852c48791957354f9fbdde8
Zell <fenglong1502@hotmail.com>
2024-03-15 08:15:34 +0800

    238 - Product of Array Except Self java solution

---
+++ 0238-Product_of_Array_Except_Self/Product_of_Array_Except_Self.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+        
+        int total = 1;
+        Set<Integer> zeroIdx = new HashSet<Integer>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0)
+                zeroIdx.add(i);
+            else
+                total *= nums[i];
+        }
+        
+        
+        if(zeroIdx.size() > 1){
+            return ans;
+        }
+        else if(zeroIdx.size() == 1){
+            List<Integer> list = new ArrayList<>(zeroIdx);
+            ans[list.get(0)] = total;
+        }
+        else{
+            for(int i = 0; i < nums.length; i++){
+                ans[i] = total / nums[i];
+            }
+        }
+        return ans;
+    }
+}
