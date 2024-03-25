a1ca05dd9928f66e52e99db79ae838933564b49b
Zell <fenglong1502@hotmail.com>
2024-03-25 08:31:13 +0800

    442 - Find all duplicates in an array java solution

---
+++ 0442-Find_All_Duplicates_in_an_Array/Find_All_Duplicates_in_an_Array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Set<Integer> ans = new HashSet<>();
+        int n = nums.length;
+        int i = 0;
+        while(i < n){
+            if(nums[i] == i + 1){
+                i++;
+            }
+            else {
+                int val = nums[i];
+                if(nums[val - 1] == val){
+                    ans.add(val);
+                    i++;
+                }
+                else {
+                    swap(nums, i, val - 1);
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+    
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
