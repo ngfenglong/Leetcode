2e034e7c5eecdb54a53736d26aa120f11a029231
Zell <fenglong1502@hotmail.com>
2024-03-30 01:45:13 +0800

    2962 - Count Subarrays where max element appear at least k times java solution

---
+++ 2962-Count_Subarrays_where_Max_Element_Appear_At_Least_K_times/Count_Subarrays_where_Max_Element_Appear_At_Least_K_times.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int maxElement = -1;
+        HashMap<Integer, List<Integer>> hm = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            maxElement = Math.max(maxElement, nums[i]);
+            if(!hm.containsKey(nums[i])) hm.put(nums[i], new ArrayList<>());
+            hm.get(nums[i]).add(i);
+        }
+        
+        List<Integer> list = hm.get(maxElement);
+        if(list.size() < k) return 0;
+        
+        int prev = -1;
+        int offset = 0;
+        long count = 0l;
+        while(offset + k <= list.size()){
+            int start = list.get(offset);
+            int end = list.get(k + offset - 1);
+            count += (long)(start - prev) * (long)(nums.length - end);
+            offset++;
+            prev = start;
+        }
+        
+        return count;
+    }
+}g
