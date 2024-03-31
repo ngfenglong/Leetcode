72a940524e61f0fcd44b8febd34e3a13bcd7c402
Zell <fenglong1502@hotmail.com>
2024-03-31 14:29:43 +0800

    2444 - Count Subarray with Fixed Bounds java solution

---
+++ 2444-Count_Subarrays_With_Fixed_Bounds/Count_Subarrays_With_Fixed_Bounds.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long count = 0l;
+        int minP = -1, maxP = -1, leftBound = -1;
+        
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i] < minK || nums[i] > maxK)
+                leftBound = i;
+            
+            if(nums[i] == minK) minP = i;
+            if(nums[i] == maxK) maxP = i;
+            
+            count += Math.max(0, Math.min(minP, maxP) - leftBound);
+        }
+        
+        
+        return count;
+    }
+}
