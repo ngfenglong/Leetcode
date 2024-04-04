e945fb94c59fa3080538bfb80ea0a73ecd0d2cc9
Zell <fenglong1502@hotmail.com>
2024-04-04 08:10:02 +0800

    1614 - Max nesting depth of the parentheses java solution

---
+++ 1614-Maximum_Nesting_Depth_of_the_Parentheses/Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int currDepth = 0;
+        
+        char[] arr = s.toCharArray();
+        for(char c: arr){
+            if(c == '('){
+                currDepth++;
+                maxDepth = Math.max(currDepth, maxDepth);
+            }
+            if(c == ')') currDepth--;
+        }
+        
+        return maxDepth;
+    }
+}
