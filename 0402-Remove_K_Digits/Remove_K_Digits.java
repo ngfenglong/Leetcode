209aee23553b59016880bae2faf954499c284a21
Zell <fenglong1502@hotmail.com>
2024-04-11 08:55:35 +0800

    401 - Remove K Digits java solution

---
+++ 0402-Remove_K_Digits/Remove_K_Digits.java
@@ -0,0 +1,34 @@
+class Solution {
+  public String removeKdigits(String num, int k) {
+   // check for special case --> second number has 0
+   // flow -> if [i] > [i + 1] --> remove i
+   // else remove from behind
+    LinkedList<Character> stack = new LinkedList<Character>();
+        
+    for(char digit : num.toCharArray()) {
+      while(stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+        stack.removeLast();
+        k -= 1;
+      }
+      stack.addLast(digit);
+    }
+        
+    /* remove the remaining digits from the tail. */
+    for(int i=0; i<k; ++i) {
+      stack.removeLast();
+    }
+        
+    // build the final string, while removing the leading zeros.
+    StringBuilder sb = new StringBuilder();
+    boolean leadingZero = true;
+    for(char digit: stack) {
+      if(leadingZero && digit == '0') continue;
+      leadingZero = false;
+      sb.append(digit);
+    }
+        
+    
+    if (sb.length() == 0) return "0";
+    return sb.toString();
+  }
+}
