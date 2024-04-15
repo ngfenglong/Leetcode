2d2827ca599d1a94ecc38c3b5f4493b86dcf0f14
Zell <fenglong1502@hotmail.com>
2024-04-15 08:13:07 +0800

    129 - Sum Root to Leaf Numbers java solution

---
+++ 0129-Sum_Root_to_Leaf_Numbers/Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,33 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        addTree(root, 0);
+        return sum;
+    }
+    
+    private void addTree(TreeNode node, int val){
+        if(node == null) return;
+        if(node.left == null && node.right == null){
+            val = (val * 10) + node.val;
+            sum += val;            
+            return;
+        }
+        
+        val = (val * 10) + node.val;
+        addTree(node.left, val);
+        addTree(node.right, val);
+    }
+}
