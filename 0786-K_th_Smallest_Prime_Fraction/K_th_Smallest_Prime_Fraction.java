2d72fa5a6998aad3839818c9381db243f9ce21fd
Zell <fenglong1502@hotmail.com>
2024-05-10 08:22:59 +0800

    0786 - K-th Smallest Prime Fraction java solution

---
+++ 0786-K_th_Smallest_Prime_Fraction/K_th_Smallest_Prime_Fraction.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        PriorityQueue<double[]> pq = new PriorityQueue<double[]>((d1,d2)->  Double.compare(d2[2], d1[2]));
+           
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                pq.add(new double[]{arr[i], arr[j], ((double)arr[i]/arr[j])});
+                if(pq.size() > k) pq.poll();
+            }
+        }
+        
+        double[] pqArr = pq.poll();
+        int[] ans = new int[]{(int)pqArr[0], (int)pqArr[1]};
+        return ans;
+    }
+}
