ff9d643d70ac07e7bfbb41ec223fa21dc6a0d97c
Zell <fenglong1502@hotmail.com>
2024-05-23 09:04:20 +0800

    2597 - The Number of Beautiful Subsets java solution

---
+++ 2597-The_Number_of_Beautiful_Subsets/The_Number_of_Beautiful_Subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int beautifulSubsets(int[] nums, int k) {
+        return countBeautifulSubsets(nums, k, 0, new ArrayList<>());
+    }
+    
+    private int countBeautifulSubsets(int[] nums, int k, int index, List<Integer> currentSubset) {
+        if (index == nums.length) {
+            if (currentSubset.isEmpty()) {
+                return 0;
+            }
+            for (int i = 0; i < currentSubset.size(); i++) {
+                for (int j = i + 1; j < currentSubset.size(); j++) {
+                    if (Math.abs(currentSubset.get(i) - currentSubset.get(j)) == k) {
+                        return 0;
+                    }
+                }
+            }
+            return 1;
+        }
+        
+        int count = 0;
+        
+        // Include the current element
+        currentSubset.add(nums[index]);
+        count += countBeautifulSubsets(nums, k, index + 1, currentSubset);
+        
+        // Exclude the current element
+        currentSubset.remove(currentSubset.size() - 1);
+        count += countBeautifulSubsets(nums, k, index + 1, currentSubset);
+        
+        return count;
+    }
+}
