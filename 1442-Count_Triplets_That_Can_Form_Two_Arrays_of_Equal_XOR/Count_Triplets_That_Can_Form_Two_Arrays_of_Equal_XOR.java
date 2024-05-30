f7d6da696043eb594681c3ac169f8acf5fc05965
Zell <fenglong1502@hotmail.com>
2024-05-30 08:45:17 +0800

    1442 improve version

--- 1442-Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR/Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.java
+++ 1442-Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR/Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.java
@@ -1,39 +1,25 @@
 class Solution {
     public int countTriplets(int[] arr) {
         // i < j <= k
-        HashMap<Integer,List<List<Integer>>> hm = new HashMap<>();
+        int[] prefixArr = new int[arr.length + 1];
+
+        prefixArr[0] = 0;
+        System.arraycopy(arr, 0, prefixArr, 1, arr.length);
+        int size = prefixArr.length;
         
-        for(int i = 0; i < arr.length; i++){
-            hm.put(i, new ArrayList<>());
-            for(int j = 0; j < i; j++){
-                List<List<Integer>> list = hm.get(j);
-                List<Integer> l = new ArrayList<>();
-                l.add(i); //
-                l.add(list.get(list.size() - 1).get(1) ^ arr[i]);
-                list.add(l);
-            }
-            
-            List<Integer> list = new ArrayList<>();
-            list.add(i);// ending index
-            list.add(arr[i]); // value
-            hm.get(i).add(list);
+        for(int i = 1; i < prefixArr.length; i++){
+            prefixArr[i] ^= prefixArr[i - 1];
         }
-        
+
         int count = 0;
-        for(int i = 0; i < arr.length - 1; i++){
-            List<List<Integer>> list1 = hm.get(i);
-            for(List<Integer> l: list1){
-                int aEndIdx = l.get(0);
-                int val = l.get(1);
-                if(aEndIdx < arr.length - 1){
-                    List<List<Integer>> list2 = hm.get(aEndIdx + 1);
-                
-                    for(List<Integer> l2: list2){
-                        if(l2.get(1) == val) count++;
-                    }    
+        for(int i = 0; i < prefixArr.length; i++){
+            for(int j = i + 1; j < prefixArr.length; j++){
+                if(prefixArr[i] == prefixArr[j]){
+                    count += (j - i - 1);    
                 }
             }
         }
+        
         
         return count;
     }
