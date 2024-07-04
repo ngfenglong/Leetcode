70427e2f8c04279f03297391e185313556814ea0
Zell <fenglong1502@hotmail.com>
2024-07-04 08:07:36 +0800

    2181 - Merge Nodes in Between Zeros java solution

---
+++ 2181-Merge_Nodes_in_Between_Zeros/Merge_Nodes_in_Between_Zeros.java
@@ -0,0 +1,31 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode newHead = new ListNode();
+        ListNode newCurr = newHead;
+        
+        ListNode curr = head;
+        curr = curr.next;
+        while(curr != null){
+            int sum = 0;
+            while(curr.val != 0){
+                sum += curr.val;
+                curr = curr.next;
+            }
+            
+            curr = curr.next;
+            
+            newCurr.next = new ListNode(sum);
+            newCurr = newCurr.next;
+        }
+        
+        return newHead.next;
+    }
+}
