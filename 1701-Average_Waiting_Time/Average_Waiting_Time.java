b42f9275d8d35347dd86b590a494d204f9307bf7
Zell <fenglong1502@hotmail.com>
2024-07-09 08:23:21 +0800

    1701 - Average Waiting Time java solution

---
+++ 1701-Average_Waiting_Time/Average_Waiting_Time.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        long currTime = 0l;
+        long waitingTime = 0l;
+     
+        
+        for(int i = 0; i < customers.length; i++){
+            if(currTime < customers[i][0]) currTime = (long)customers[i][0];
+            
+            currTime += (long)customers[i][1];
+            waitingTime += currTime - customers[i][0];
+        }
+        
+        return (double)waitingTime / customers.length;   
+    }
+}
