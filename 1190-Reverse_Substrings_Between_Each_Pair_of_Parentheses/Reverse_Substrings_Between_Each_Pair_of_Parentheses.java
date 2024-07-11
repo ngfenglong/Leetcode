bce5a1a519d5c20a5165bcced1448a52d9034823
Zell <fenglong1502@hotmail.com>
2024-07-11 08:18:09 +0800

    1190 - Reverse Substrings Between Each Pair of Parentheses java solution

---
+++ 1190-Reverse_Substrings_Between_Each_Pair_of_Parentheses/Reverse_Substrings_Between_Each_Pair_of_Parentheses.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<String> stack = new Stack<>();
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(sb.toString());
+                sb = new StringBuilder();
+            }
+            else if(c == ')'){
+                String str = reverseString(sb.toString());                
+                sb = new StringBuilder();                
+                if(!stack.isEmpty()) sb.append(stack.pop());
+                sb.append(str);
+            }
+            else {
+                sb.append(c);
+            }
+        }
+        
+        if(sb.length() > 0){
+            stack.push(sb.toString());
+        }
+        
+        Stack<String> tempStack = new Stack<>();
+        while(!stack.isEmpty()){
+            tempStack.push(stack.pop());
+        }
+        
+        sb = new StringBuilder();
+        while(!tempStack.isEmpty()){
+            sb.append(tempStack.pop());
+        }
+        
+        return sb.toString();
+            
+    }
+    
+    private String reverseString(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i = s.length() - 1; i >= 0; i--){
+            sb.append(s.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
