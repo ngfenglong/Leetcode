47e59efc37a02503e838dd93ec1f6c74bb368901
Zell <fenglong1502@hotmail.com>
2024-08-12 08:09:11 +0800

    703 - Kth Largest Element in a Stream java solution

---
+++ 0703-Kth_Largest_Element_in_a_Stream/Kth_Largest_Element_in_a_Stream.java
@@ -0,0 +1,19 @@
+class KthLargest {
+    PriorityQueue<Integer> queue = new PriorityQueue<>();
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+            
+        for(int i = 0; i < nums.length; i++){
+            queue.add(nums[i]);
+            if(queue.size() > k) queue.poll();
+        }
+    }
+    
+    public int add(int val) {
+        queue.add(val);
+        if(queue.size() > k) queue.poll();
+        
+        return queue.peek();
+    }
+}
