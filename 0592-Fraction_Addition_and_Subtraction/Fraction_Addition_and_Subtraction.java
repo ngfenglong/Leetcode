0a2f641e4361f19159f34b08ed1db35aa4e65418
Zell <fenglong1502@hotmail.com>
2024-08-23 08:34:40 +0800

    Fraction Addition and Subtraction java solution

---
+++ 0592-Fraction_Addition_and_Subtraction/Fraction_Addition_and_Subtraction.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String fractionAddition(String expression) {
+        int numerator = 0;
+        int denominator = 1;
+        
+        int i = 0;
+        while(i < expression.length()){
+            boolean isPositive = true;
+            
+            // Handle sign
+            if(expression.charAt(i) == '+' || expression.charAt(i) == '-'){
+                if(expression.charAt(i) == '-') isPositive = false;
+                i++;
+            }
+            
+            int localNumerator = 0;
+            int localDenominator = 0;
+            while(Character.isDigit(expression.charAt(i))){
+                localNumerator *= 10;
+                localNumerator += (expression.charAt(i) - '0');
+                i++;
+            }
+            
+            if(!isPositive) localNumerator *= -1;
+            
+            // Skip divisor
+            i++;
+            while(i < expression.length() && Character.isDigit(expression.charAt(i))){
+                localDenominator *= 10;
+                localDenominator += (expression.charAt(i) - '0');
+                i++;
+            }
+            
+            numerator = numerator * localDenominator + localNumerator * denominator;
+            denominator = denominator * localDenominator;
+        }
+        
+        
+        int divisor = Math.abs(findCommonDivisor(numerator, denominator));
+        numerator /= divisor;
+        denominator /= divisor;
+        
+        return numerator + "/" + denominator;
+    }
+    
+    private int findCommonDivisor(int a, int b) {
+        if(a == 0) return b;
+        
+        return findCommonDivisor(b % a, a);
+    }
+}
