117c5571434048d787da86cc4d6bad06510627b6
Zell <fenglong1502@hotmail.com>
2024-08-26 08:07:21 +0800

    590 N-ary Tree Postorder traversal java solution

---
+++ 0590-N_ary_Tree_Postorder_Traversal/N_ary_Tree_Postorder_Traversal.java
@@ -0,0 +1,34 @@
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> postorder(Node root) {
+        traverse(root);
+        return ans;
+    }
+    
+    private void traverse(Node root){
+        if(root == null) return;
+        if(root.children != null){
+            for(Node n : root.children){
+                traverse(n);
+            }    
+        }
+        
+        ans.add(root.val);
+    }
+}
