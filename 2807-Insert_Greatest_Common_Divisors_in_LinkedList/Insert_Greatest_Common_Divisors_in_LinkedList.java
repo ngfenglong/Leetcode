3e59e6d2e7bbe5e446216299613c1e5466fba1e9
Zell <fenglong1502@hotmail.com>
2024-09-10 08:30:40 +0800

    2807 - Insert Greatest Common Divisors in Linked List

---
+++ 2807-Insert_Greatest_Common_Divisors_in_LinkedList/Insert_Greatest_Common_Divisors_in_LinkedList.java
@@ -0,0 +1,30 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode curr = head;
+        while(curr != null && curr.next != null){
+            int divisor = gcd(curr.val, curr.next.val);
+            curr.next = new ListNode(divisor, curr.next);
+            curr = curr.next.next;
+        }
+        
+        return head;
+    }
+    
+    private int gcd(int i1, int i2){
+        while(i2 != 0){
+            int temp = i2;
+            i2 = i1 % i2;
+            i1 = temp;
+        }
+        
+        return i1;
+    }
+}
