2ca5122077fb12ac7c4e26946f3b9d08212b7dd1
Zell <fenglong1502@hotmail.com>
2024-09-23 08:27:59 +0800

    2707 - Extra characters in a string java solution

---
+++ 2707-Extra_Characters_in_a_String/Extra_Characters_in_a_String.java
@@ -0,0 +1,31 @@
+class Solution {
+    Set<String> dictSet = new HashSet<>();
+    int minCharacters = Integer.MAX_VALUE;
+    Integer[] memo;
+    
+    public int minExtraChar(String s, String[] dictionary) {
+        memo = new Integer[s.length()];
+        
+        for(String word: dictionary){
+            dictSet.add(word);
+        }
+        
+        return checkExtra(s, 0);
+    }
+    
+    private int checkExtra(String s, int i){
+        if(i >= s.length()) return 0;
+            
+        if(memo[i] != null) return memo[i];
+        
+        int localMin = checkExtra(s, i + 1) + 1;
+        for(int j = i + 1; j < s.length() + 1; j++){
+            if(dictSet.contains(s.substring(i, j))) {
+                localMin = Math.min(localMin, checkExtra(s, j));
+            }
+        }
+                
+        memo[i] = localMin;
+        return localMin;
+    }
+}
