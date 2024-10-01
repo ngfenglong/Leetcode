c1ac56251b6a8694eae1df18a785f43c6641e8e0
Zell <fenglong1502@hotmail.com>
2024-10-01 08:51:39 +0800

    1497 - Check if Array Pairs are Diviible by K

---
+++ 1497-Check_if_Array_Pairs_Are_Divisible_by_K/Check_if_Array_Pairs_Are_Divisible_by_K.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {     
+        HashMap<Integer, Integer> elements = new HashMap<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            int element = arr[i] % k;
+            if(element < 0) element = element + k;
+            
+            elements.put(element, elements.getOrDefault(element, 0) + 1);
+        }
+        
+        
+        for(Integer element: elements.keySet()){
+            if(element == 0 || element * 2 == k){
+                if(elements.get(element) % 2 != 0) return false;
+            }
+            else {
+                if(!elements.containsKey(k - element) || !elements.get(element).equals(elements.get(k - element))) return false;
+            }            
+        }
+        
+        
+        return true;
+    }
+}
