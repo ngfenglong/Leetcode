5e9ba49be1747f148ce86c0b035025ef922e68ba
Zell <fenglong1502@hotmail.com>
2024-10-10 08:36:43 +0800

    962 Maximum Width Ramp java solution

---
+++ 0962-Maximum_With_Ramp/Maximum_Width_Ramp.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        Stack<Integer> monoStack = new Stack<>();
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            if(monoStack.isEmpty()) monoStack.push(i);
+            else {
+                if(nums[i] < nums[monoStack.peek()]) monoStack.push(i); 
+            }
+        }
+        
+        int maxWidth = 0;
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(monoStack.isEmpty()) return maxWidth;
+            
+            while(!monoStack.isEmpty() && nums[monoStack.peek()] <= nums[i]){
+                int idx = monoStack.pop();
+                maxWidth = Math.max(maxWidth, i - idx);
+            }
+        }
+        
+        return maxWidth;
+    }
+}
