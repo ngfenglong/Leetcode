1b02cd81eef2cae168001c80994a7ce0da00f9d2
Zell <fenglong1502@hotmail.com>
2024-11-15 08:20:10 +0800

    171 - Excel Sheet Column Number java solution

---
+++ 0171-Excel_Sheet_Column_Number/Excel_Sheet_Column_Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int cols = 0;
+        
+        for(int i = columnTitle.length() - 1; i >= 0; i--){
+            int pow = (columnTitle.length() - 1 - i);            
+            int temp = (int)Math.pow(26, pow) * ((columnTitle.charAt(i) - 'A') + 1);
+            cols += temp;
+        }
+        
+        return cols;
+    }
+}
