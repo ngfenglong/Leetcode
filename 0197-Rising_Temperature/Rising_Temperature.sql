9026749bd8acd54733e9820db327f6a6a1933f7e
Zell <fenglong1502@hotmail.com>
2024-11-22 10:49:11 +0800

    197 - Rising Temperature SQL Solution

---
+++ 0197-Rising_Temperature/Rising_Temperature.sql
@@ -0,0 +1,6 @@
+-- Write your MySQL query statement below
+SELECT id FROM Weather w1
+WHERE w1.temperature > (
+    SELECT temperature FROM Weather w2 
+    WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+)
