20d83cf59ea79ffdeb6ba22b91ba0e6999135f73
Zell <fenglong1502@hotmail.com>
2024-11-27 08:32:05 +0800

    Shortest Distance After Road Addition Queries java solution

---
+++ 3243-Shortest_Distance_After_Road_Addition_Queries_I/Shortest_Distance_After_Road_Addition_Queries_I.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        int[] ans = new int[queries.length];
+        int[] dp = new int[n];
+        
+        HashMap<Integer, List<Integer>> hm = new HashMap<>();
+        hm.put(n - 1, new ArrayList<>());        
+        for(int i = n - 2; i >= 0; i--){
+            dp[i] = dp[i + 1] + 1;
+            hm.put(i, new ArrayList<>());
+            hm.get(i).add(i + 1);
+        }
+        
+        for(int i = 0; i < queries.length; i++){
+            int from = queries[i][0];
+            int to = queries[i][1];
+            
+            hm.get(from).add(to);
+            
+            for(int j = from; j >= 0; j--){
+                for(Integer p: hm.get(j)){
+                    dp[j] = Math.min(dp[j], dp[p] + 1);
+                }
+            }
+            
+            ans[i] = dp[0];
+        }
+        
+        return ans;
+    }
+}
