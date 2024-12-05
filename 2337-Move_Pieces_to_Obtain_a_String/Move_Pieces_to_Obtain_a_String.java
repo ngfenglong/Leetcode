279d13612e28afda61510baa34d13dd9db710fb9
Zell <fenglong1502@hotmail.com>
2024-12-05 08:36:04 +0800

    2337 - Move Pieces to Obtain a String java solution

---
+++ 2337-Move_Pieces_to_Obtain_a_String/Move_Pieces_to_Obtain_a_String.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        int n = start.length();
+        int sPointer = 0;
+        int tPointer = 0;
+        
+        // Check L 
+        while(tPointer < n || sPointer < n){
+            while(tPointer < n && target.charAt(tPointer) == '_') tPointer++;
+            
+            while(sPointer < n && start.charAt(sPointer) == '_') sPointer++;
+            
+            // If traverse through on side, check if the other side still has an element or not
+            if(sPointer == n || tPointer == n) return sPointer == tPointer;
+            
+            if(start.charAt(sPointer) != target.charAt(tPointer) ||
+               (target.charAt(tPointer) == 'L' && sPointer < tPointer) ||
+               (target.charAt(tPointer) == 'R' && tPointer < sPointer)
+              ){
+                 return false;
+            }
+            
+            sPointer++;
+            tPointer++;
+        }
+        
+        return true;
+    }
+}
