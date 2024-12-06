75aff5c4474db6af4b994fd5d09736ee0d8446bd
Zell <fenglong1502@hotmail.com>
2024-12-06 08:24:08 +0800

    2554 - Maximum Number of Integers to Choose From a Range I java solution

---
+++ 2554-Maximum_Number_of_Integers_to_Choose_From_a_Range_I/Maximum_Number_of_Integers_to_Choose_From_a_Range_I.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Set<Integer> bannedSet = new HashSet<>();
+        for(int i: banned){
+            bannedSet.add(i);
+        }
+        
+        int count = 0;
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++){
+            if(!bannedSet.contains(i)){
+                sum += i;
+                if(sum > maxSum) return count;
+                
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
