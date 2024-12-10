be0cffb951253c0bb5c67c63032df9006d249d8b
Zell <fenglong1502@hotmail.com>
2024-12-10 08:18:38 +0800

    2981 - FInd Longest Special Substring that occurs Thrice I java solution

---
+++ 2981-Find_Longest_Special_Substring_That_Occurs_Thrice_I/Find_Longest_Special_Substring_That_Occurs_Thrice_I.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumLength(String s) {
+        int[][] specialCount = new int[26][s.length() + 1];
+        
+        int prev = -1;
+        int count = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            int curr = s.charAt(i) - 'a';
+            if(prev != curr) count = 0;
+            
+            count++;
+            
+            for(int j = 0; j < count; j++){
+                specialCount[curr][j]++;
+            }
+            
+            prev = curr;
+        }
+        
+        int longest = -1;
+        
+        for(int i = 0; i < 26; i++){
+            for(int j = 0; j < specialCount[i].length; j++){
+                if(specialCount[i][j] == 0) break;
+                if(specialCount[i][j] >= 3 && j >= longest) {
+                    longest = j + 1;  
+                } 
+            }
+        }
+        
+        return longest;
+    }
+}
