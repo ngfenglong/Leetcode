81c22d00405a765695e6437b3a4635c516cc421e
Zell <fenglong1502@hotmail.com>
2024-12-18 08:11:29 +0800

    1475 - Final Prices with a Special Discount in a Shop java solution

---
+++ 1475-Final_Prices_With_a_Special_Discount_in_a_Shop/Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = new int[n];
+        
+        ans[n-1] = prices[n-1];
+        
+        for(int i = n - 2; i >= 0; i--){
+            ans[i] = prices[i];
+            
+            int j = i + 1;
+            while(j < n && prices[j] > prices[i]) j++;
+            
+            if(j < n) ans[i] -= prices[j];
+        }
+        
+        return ans;
+    }
+}
