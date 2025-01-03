9b10d8326d0cd42bf0e772e5a71579a9403be43a
Zell <fenglong1502@hotmail.com>
2025-01-04 00:50:52 +0800

    2270 - Number of ways to Split Array java solution

---
+++ 2270-Number_of_Ways_to_Split_Array/Number_of_Ways_to_Split_Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long[] sumup = new long[nums.length];
+
+        sumup[0] = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            sumup[i] += sumup[i - 1] + nums[i];
+        }
+
+        int count = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(sumup[i] >= sumup[nums.length - 1] - sumup[i]) count++;
+        }
+
+        return count;
+    }
+}
