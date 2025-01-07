7ffabfd7ba3e1c9f5f22afb313a0d4d499c26136
Zell <fenglong1502@hotmail.com>
2025-01-07 08:17:47 +0800

    1408 - String Matching in an Array java solution

---
+++ 1408-String_Matching_in_an_Array/String_Matching_in_an_Array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> matching = new ArrayList<>();
+
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < words.length; j++){
+                if(i != j){
+                    String word = words[j];
+                    String toCheck = words[i];
+
+                    if(word.indexOf(toCheck) != -1) {
+                        matching.add(toCheck);
+                        break;
+                    }
+                }
+            }
+        }   
+
+        return matching;
+    }
+}
