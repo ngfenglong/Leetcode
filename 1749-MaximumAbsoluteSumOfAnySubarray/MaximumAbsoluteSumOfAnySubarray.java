fa82fe5d75a4b7a85122f544baffca4796c11b99
Zell <fenglong1502@hotmail.com>
2025-02-26 08:25:08 +0800

    1749 - Maximum Absolute Sum of Any Subarray java solution

--- 1749-MaximumAbsoluteSumOfAnySubarray/MaximumAbsoluteSumOfAnySubarray.java
+++ 1749-MaximumAbsoluteSumOfAnySubarray/MaximumAbsoluteSumOfAnySubarray.java
@@ -1,16 +1,18 @@
-class Solution {
-    public int maxAbsoluteSum(int[] nums) {
-        int minPrefixSum = 0;
-        int maxPrefixSum = 0;
-        int prefixSum = 0;
+public class MaximumAbsoluteSumOfAnySubarray {
+    class Solution {
+        public int maxAbsoluteSum(int[] nums) {
+            int minPrefixSum = 0;
+            int maxPrefixSum = 0;
+            int prefixSum = 0;
+        
+            for(int i = 0; i < nums.length; i++){
+                prefixSum += nums[i];
     
-        for(int i = 0; i < nums.length; i++){
-            prefixSum += nums[i];
-
-            minPrefixSum = Math.min(prefixSum, minPrefixSum);
-            maxPrefixSum = Math.max(prefixSum, maxPrefixSum);
+                minPrefixSum = Math.min(prefixSum, minPrefixSum);
+                maxPrefixSum = Math.max(prefixSum, maxPrefixSum);
+            }
+        
+            return maxPrefixSum - minPrefixSum;
         }
-    
-        return maxPrefixSum - minPrefixSum;
     }
 }
