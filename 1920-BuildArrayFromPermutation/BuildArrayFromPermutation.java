2917ae1996cdcb08525df292c72980d792aaa248
Zell <fenglong1502@hotmail.com>
2025-05-06 10:53:09 +0800

    1920 Build Array From Permutation java solution

---
+++ 1920-BuildArrayFromPermutation/BuildArrayFromPermutation.java
@@ -0,0 +1,19 @@
+public class BuildArrayFromPermutation {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+            int[] ans = new int[nums.length];
+
+            for (int i = 0; i < nums.length; i++) {
+                ans[i] = nums[nums[i]];
+            }
+
+            return ans;
+        }
+
+        private int recursive(int[] nums, int i){
+            if(nums[i] == nums[nums[i]]) return nums[i];
+    
+            return 
+        }
+    }
+}
