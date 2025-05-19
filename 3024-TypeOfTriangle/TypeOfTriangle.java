b3a46be636d993e7537efaa9fa892ece79dd6fc6
Zell <fenglong1502@hotmail.com>
2025-05-19 08:16:14 +0800

    3024 Type of Triangle

---
+++ 3024-TypeOfTriangle/TypeOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TypeOfTriangle {
+    class Solution {
+        public String triangleType(int[] nums) {
+            Arrays.sort(nums);
+            if (nums[0] + nums[1] <= nums[2]) {
+                return "none";
+            } else if (nums[0] == nums[1] && nums[1] == nums[2])
+                return "equilateral";
+            else if (nums[0] == nums[1] || nums[1] == nums[2])
+                return "isosceles";
+            else
+                return "scalene";
+        }
+    }
+}
